977a17fd89f599e7f05a86e056e77701823da1da
Giorgos Logiotatidis <seadog@sealabs.net>
2017-09-07 22:51:21 +0200

    Use gh-pages.

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-git clone https://github.com/glogiotatidis/planet.mozillareps.org.git
+git clone -b gh-pages https://github.com/glogiotatidis/planet.mozillareps.org.git
 pushd planet.mozillareps.org
 git config user.email "planetupdate@mozillareps.org"
 git config user.name "planet-update-bot"
 git remote add origin-rw https://${GITHUB_AUTH_TOKEN}@github.com/glogiotatidis/planet.mozillareps.org.git
 
 docker run \
-       -v `pwd`/docs:/data/output \
+       -v `pwd`:/data/output \
        -v ~/cache:/data/cache \
        giorgos/planet-mozillareps-org-builder
-git add -f --all docs/
+git add -f --all .
 git commit -m "Site update"
 
 # Push only if there are commit changes.
 if [[ $? == 0 ]];
 then
-    git push origin-rw master 2> /dev/null
+    git push origin-rw gh-pages 2> /dev/null
 fi
