9f9ab773d9659659ebb3f6522df1acf26f861131
Giorgos Logiotatidis <seadog@sealabs.net>
2017-09-07 22:10:44 +0200

    Use docker volumes.

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
-docker run giorgos/planet-mozillareps-org-builder
-docker cp $(docker ps -a -q | head -n 1):/data/output .
-
 git clone https://github.com/glogiotatidis/planet.mozillareps.org.git
-rsync --recursive --delete ./output/ planet.mozillareps.org/docs/
-
-pushd planet.mozillareps.org/
-
 git config user.email "planetupdate@mozillareps.org"
 git config user.name "planet-update-bot"
 git remote add origin-rw https://${GITHUB_AUTH_TOKEN}@github.com/glogiotatidis/planet.mozillareps.org.git
+
+docker run \
+       -v `pwd`/planet.mozillareps.org/docs:/data/output \
+       -v ~/cache:/data/cache \
+       giorgos/planet-mozillareps-org-builder
+
 git add -f --all docs/
 git commit -m "Site update"
 
