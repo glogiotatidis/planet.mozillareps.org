60b7ddac3d172231983c54bf3c3f11ab9b9d4b16
Giorgos Logiotatidis <seadog@sealabs.net>
2017-09-13 16:43:27 +0300

    Change from glogiotatidis to mozilla-parsys

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-git clone -b gh-pages https://github.com/glogiotatidis/planet.mozillareps.org.git
+git clone -b gh-pages https://github.com/mozilla-parsys/planet.mozillareps.org.git
 pushd planet.mozillareps.org
 git config user.email "planetupdate@mozillareps.org"
 git config user.name "planet-update-bot"
-git remote add origin-rw https://${GITHUB_AUTH_TOKEN}@github.com/glogiotatidis/planet.mozillareps.org.git
+git remote add origin-rw https://${GITHUB_AUTH_TOKEN}@github.com/mozilla-parsys/planet.mozillareps.org.git
 
 docker run \
        -v `pwd`:/data/output \
        -v ~/cache:/data/cache \
-       giorgos/planet-mozillareps-org-builder
+       mozilla-parsys/planet-mozillareps-org-builder
 git add -f --all .
 git commit -m "Site update"
 
